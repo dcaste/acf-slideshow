38e1132161d6eefa131762723bcf6d8690758dfe
unknown <daxcastellon@gmail.com>
2019-07-05 10:39:59 -0600

    Changed language and  refactored  conditionals

--- acf-slideshow.php
+++ acf-slideshow.php
@@ -25,6 +25,7 @@ if( !empty($block['align']) ) {
 ?>
 
 <?php
+// If there are slides.
 if ( have_rows( 'acf_slideshow' ) ) :
 ?>
 
@@ -33,58 +34,41 @@ if ( have_rows( 'acf_slideshow' ) ) :
 		<?php
 		while ( have_rows( 'acf_slideshow' ) ) :
 			the_row();
-
-      // Get image sizes.
-			$image = get_sub_field( 'image' );
-			if ( $image ) {
-				$image_md   = $image['sizes']['medium'];
-				$image_ml   = $image['sizes']['medium_large'];
-				$image_lg   = $image['sizes']['large'];
-				$image_sli  = $image['sizes']['slideshow'];
-				$image_full = $image['url'];
-				$image_alt  = $image['alt'];
-				$image_alt  = ( $image_alt ) ? $image_alt : 'Fallback ALT text goes here';
-			}
-
-      // If 
-			$link_add = get_sub_field( 'link_add' );
-			if ( true === $link_add ) {
-				$link_tipo = get_sub_field( link_tipo' );
-				$link      = ( 'interno' == $link_tipo ) ? get_sub_field( link_int' ) : get_sub_field( link_ext' );
-				$target 		 = get_sub_field( 'target' );
-			}
-
-			$overlay = get_sub_field( 'overlay_add' );
-			if ( true === $overlay ) {
-				$overlay_color = ( get_sub_field( 'overlay_color' ) ) ? get_sub_field( 'overlay_color' ) : '#3a4790';
-				$overlay_opac  = ( get_sub_field( 'overlay_opac' ) ) ? get_sub_field( 'overlay_opac' ) : 0.05;
-			}
-
-			$boton = get_sub_field( 'agregar_boton' );
 		?>
 
 			<div class="acf-slideshow__item">
 				<div class="acf-slideshow__container">
 
 					<?php
-					if ( get_sub_field( 'image' ) ) : // Si existe image:
+					// If image exists.
+					if ( get_sub_field( 'image' ) ) :
+
+						$image      = get_sub_field( 'image' );
+						$image_md   = $image['sizes']['medium'];
+						$image_ml   = $image['sizes']['medium_large'];
+						$image_lg   = $image['sizes']['large'];
+						$image_sli  = $image['sizes']['slideshow'];
+						$image_full = $image['url']; // Fallback image.
+						$image_alt  = $image['alt'];
+						$image_alt  = ( $image_alt ) ? $image_alt : 'Fallback ALT text goes here';
 					?>
 
 						<picture class="acf-slideshow__image">
-							<source media="(max-width: 480px)" srcset="<?php echo esc_url( $image_md ); ?>">
-							<source media="(max-width: 768px)" srcset="<?php echo esc_url( $image_ml ); ?>">
+							<source media="(max-width: 480px)"  srcset="<?php echo esc_url( $image_md ); ?>">
+							<source media="(max-width: 768px)"  srcset="<?php echo esc_url( $image_ml ); ?>">
 							<source media="(max-width: 1024px)" srcset="<?php echo esc_url( $image_lg ); ?>">
 							<source media="(max-width: 1280px)" srcset="<?php echo esc_url( $image_sli ); ?>">
 							<img src="<?php echo esc_url( $image_full ); ?>" alt="<?php echo esc_html( $image_alt ); ?>" >
 						</picture>
 
-					<?php endif; // Finaliza si hay image. ?>
+					<?php endif; // Ends if image exists. ?>
 
 					<?php
-					if ( get_sub_field( 'headline' ) || get_sub_field( 'tagline' ) ) : // Si existe contenido:
+					// If content exists:
+					if ( get_sub_field( 'headline' ) || get_sub_field( 'tagline' ) ) :
 					?>
 
-						<div class="acf-slideshow__contenido">
+						<div class="acf-slideshow__content">
 
 							<?php if ( get_sub_field( 'headline' ) ) { ?>
 								<h2 class="acf-slideshow__headline">
@@ -98,28 +82,32 @@ if ( have_rows( 'acf_slideshow' ) ) :
 								</div>
 							<?php } ?>
 
-							<?php if ( true === $boton ) { ?>
-								<p class="acf-slideshow__area-boton">
-									<a href="<?php the_sub_field( 'boton_url' ); ?>" class="boton-blanco">
+							<?php if ( true === get_sub_field( 'add_button' ) ) { ?>
+								<div class="acf-slideshow__button-area">
+									<a href="<?php the_sub_field( 'boton_url' ); ?>" class="button">
 										<?php the_sub_field( 'boton_copy' ); ?>
 									</a>
-								</p>
+								</div>
 							<?php } ?>
 
 						</div>
 
-					<?php endif; // Finaliza si hay contenido. ?>
+					<?php endif; // Ends if content exists. ?>
 
 					<?php
-					if ( true === $overlay ) { // Si existe overlay:
+					// If we want to use an overlay get overlay color, opacity and their fallback values.
+					if ( true === get_sub_field( 'overlay_add' ) ) {
+						$overlay_color   = ( get_sub_field( 'overlay_color' ) ) ? get_sub_field( 'overlay_color' ) : '#3a4790'; 
+						$overlay_opacity = ( get_sub_field( 'overlay_opacity' ) ) ? get_sub_field( 'overlay_opacity' ) : 0.05;
 					?>
-						<div class="acf-slideshow__overlay" style="background-color:<?php echo esc_attr( $overlay_color ); ?>; opacity: <?php echo esc_attr( $overlay_opac ); ?>">
-							<span></span>
-						</div>
+						<div class="acf-slideshow__overlay" style="background-color:<?php echo esc_attr( $overlay_color ); ?>; opacity: <?php echo esc_attr( $overlay_opac ); ?>"><span></span></div>
 					<?php } ?>
 
 					<?php
-					if ( true === $link_add ) {	// Si existe link:
+					// If we want to add a link gets the internal or external link and the link target.
+					if ( true === get_sub_field( 'link_add' ) ) {
+						$link   = ( 'local' == get_sub_field( 'link_type' ) ) ? get_sub_field( 'link_local' ) : get_sub_field( 'link_external' );
+						$target = get_sub_field( 'target' );
 					?>
 						<a class="acf-slideshow__link" href="<?php echo esc_url( $link ); ?>" target="<?php echo esc_attr( $target ) ?>"><span></span></a>
 					<?php } ?>
@@ -134,4 +122,4 @@ if ( have_rows( 'acf_slideshow' ) ) :
 	</section>
 
 <?php
-endif; // Finaliza si existen campos en el slideshow.
+endif; // Ends if there are slides.
